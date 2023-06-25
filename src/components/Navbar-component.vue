f21bd63a49587c8f75b7e5362e49733077661715
Sergio Bazan <drakeb2008@hotmail.com>
2023-06-25 12:31:15 -0500

    add buttons

--- src/components/Navbar-component.vue
+++ src/components/Navbar-component.vue
@@ -22,8 +22,8 @@
     </div>
 
     <div v-else>
-      <button on-click="">Login</button>
-      <button>Register</button>
+      <button @click="goToLogin()">Login</button>
+      <button @click="goToRegister()">Register</button>
     </div>
   </nav>
 </template>
@@ -54,6 +54,12 @@ export default {
 
       //location.reload()
       this.$router.push("/");
+    },
+    goToLogin() {
+      this.$router.push("/login");
+    },
+    goToRegister() {
+      this.$router.push("/register");
     },
   },
 };
